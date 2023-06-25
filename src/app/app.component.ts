595186e147d975894cadce4b066ac719f3966ce3
Dany Paredes <danywalls4@gmail.com>
2023-06-25 21:24:32 +0200

    chore: format code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,33 @@
-import {Component, inject} from '@angular/core';
-import {BreadCrumbService} from "./services/breadcrumb.service";
-import {Router} from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { BreadCrumbService } from './services/breadcrumb.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    template: `
-        <app-navigation></app-navigation>
-        <section>
-            <ng-container *ngIf="items$ | async as items">
-                <kendo-breadcrumb (itemClick)="onItemClick($event)" [items]="items"></kendo-breadcrumb>
-            </ng-container>
-        </section>
-        <router-outlet></router-outlet>
-        <footer>
-            <span>2023</span>
-        </footer>
-    `,
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  template: `
+    <app-navigation></app-navigation>
+    <section>
+      <ng-container *ngIf="items$ | async as items">
+        <kendo-breadcrumb
+          (itemClick)="onItemClick($event)"
+          [items]="items"
+        ></kendo-breadcrumb>
+      </ng-container>
+    </section>
+    <router-outlet></router-outlet>
+    <footer>
+      <span>2023</span>
+    </footer>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  #breadcrumbService = inject(BreadCrumbService);
+  items$ = this.#breadcrumbService.items$;
+  #router = inject(Router);
 
-    #breadcrumbService = inject(BreadCrumbService)
-    public items$ = this.#breadcrumbService.items$
-    #router = inject(Router);
-
-
-    public onItemClick(item: any) {
-        this.#breadcrumbService.updateBreadcrumb(item)
-        this.#router.navigate([item.path]);
-    }
+  public onItemClick(item: any) {
+    this.#breadcrumbService.updateBreadcrumb(item);
+    this.#router.navigate([item.path]);
+  }
 }
